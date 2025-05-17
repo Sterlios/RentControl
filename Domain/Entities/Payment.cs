c0b15a504f21cfbc73ba984e4bd50e7d90c83f37
Sterlios <kuzmin.s.anton@gmail.com>
2025-05-17 20:57:13 +0200

    Add Payment Logic

--- Domain/Entities/Payment.cs
+++ Domain/Entities/Payment.cs
@@ -10,9 +10,37 @@ namespace Domain.Entities
         public DateOnly InvoiceDate { get; private set; }
         public DateOnly ExpiredDate { get; private set; }
         public Money Money { get; private set; }
+        public string InvoiceLink { get; private set; }
         public PaymentStatus Status { get; private set; }
         public DateOnly PaidDate { get; private set; }
-        public string InvoiceLink { get; private set; }
         public string BillLink { get; private set; }
+
+        public Payment(PaymentCategory category, DateOnly invoiceDate, DateOnly expiredDate, Money money, string invoiceLink)
+        {
+            Category = category;
+            InvoiceDate = invoiceDate;
+            ExpiredDate = expiredDate;
+            Money = money;
+            InvoiceLink = invoiceLink;
+            Status = PaymentStatus.Unpaid;
+        }
+
+        public void SetId(Guid id) =>
+            Id = id;
+
+        public void MarkAsPaid(DateOnly paidDate, string billLink)
+        {
+            if (Status == PaymentStatus.Paid)
+                throw new InvalidOperationException("Payment is already paid");
+
+            Status = PaymentStatus.Paid;
+            PaidDate = paidDate;
+            BillLink = billLink;
+        }
+
+        public void MarkAsOverdue()
+        {
+            if (DateTime.Today > DateOnly.ToDateTime(ExpiredDate))
+        }
     }
 }
